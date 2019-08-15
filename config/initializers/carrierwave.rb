28b9ea737dcbee34428c885deb5b7ac60c74101b
Jessi Calicotte <orgkleen@gmail.com>
2019-08-15 23:36:34 +0000

    added bucket and AWS keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.storage    = :fog
-  config.aws_bucket = ENV["AWS_BUCKET"]
+  config.aws_bucket = ENV["flixterjcalicot"]
   config.aws_acl    = "public_read"
 
   config.aws_credentials = {
-    access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    secret_access_key: ENV["AWS_SECRET_KEY"],        # required
-    region:            ENV["AWS_REGION"]
+    access_key_id:     ENV["AKIASZV6FXWXLEIUOXJV"],        # required
+    secret_access_key: ENV["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"],        # required
+    region:            ENV["US East(N.Virgina"]
   }
 end
