00c7e2602621914faeeb88dadbdec67fdee8bb70
Jessi Calicotte <orgkleen@gmail.com>
2019-08-17 20:24:42 +0000

    requiring users to be enrolled in a course to view lessons

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -13,6 +13,7 @@ class Instructor::SectionsController < ApplicationController
 
 
       private
+      
         def require_authorized_for_current_course
     if current_course.user != current_user
       render plain: "Unauthorized", status: :unauthorized
