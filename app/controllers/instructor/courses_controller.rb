6378f111997a3a88a2c9378a7c29fc058a6373b4
Jessi Calicotte <orgkleen@gmail.com>
2019-08-18 01:42:42 +0000

    working through error after error

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,31 +1,30 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_course, only: [:show]
-
+  
   def new
     @course = Course.new
   end
-x
+
   def create
     @course = current_user.courses.create(course_params)
     if @course.valid?
-     redirect_to instructor_course_path(@course)
-   else
-     render :new, status: :unprocessable_entity
+      redirect_to instructor_course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-   def show
-   end
-
+  def show
+  end
 
   private
 
-  def require_authorized_for_current_course
-    if current_course.user != current_user
-      render plain: "Unauthorized", status: :unauthorized
+    def require_authorized_for_current_course
+      if current_course.user != current_user
+        render plain: "Unauthorized", status: :unauthorized
+      end
     end
-  end
 
   helper_method :current_course
   def current_course
@@ -35,5 +34,4 @@ x
   def course_params
     params.require(:course).permit(:title, :description, :cost, :image)
   end
-
 end
