3ed9f0cb3081a8f4dbe444d73b5b2094c0dcd9e0
Jessi Calicotte <orgkleen@gmail.com>
2019-08-22 03:21:39 +0000

    adding sections

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::SectionsController < ApplicationController
       before_action :authenticate_user!
-      before_action :require_authorized_for_current_course
+      before_action :require_authorized_for_current_course, only: [:create]
+      before_action :require_authorized_for_current_section, only: [:update]
     def new
     @section = Section.new
   end
@@ -11,25 +12,36 @@ class Instructor::SectionsController < ApplicationController
   end
 
 
+
 def current_section
     @current_section ||= Section.find(params[:id])
+  end
+
+def update
+    current_section.update_attributes(section_params)
+    render plain: 'section updated!'
   end
 
 
       private
       
         def require_authorized_for_current_course
-    if current_course.user != current_user
-      render plain: "Unauthorized", status: :unauthorized
-    end
+           if current_course.user != current_user
+              render plain: "Unauthorized", status: :unauthorized
+           end
+      end
+
+
+
+     helper_method :current_course
+        def current_course
+          if params[:course_id]
+            @current_course ||= Course.find(params[:course_id])
+          else
+          current_section.course
   end
+end
 
-
-
-      helper_method :current_course
-  def current_course
-    @current_course ||= Course.find(params[:course_id])
-  end
 
       def section_params
         params.require(:section).permit(:title, :row_order_position)
