d988deea75b1e43eaf272a3ec6b54b2dc669ab9b
Jessi Calicotte <orgkleen@gmail.com>
2019-08-29 02:19:47 +0000

    working on creating a new lesson

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,9 @@ Rails.application.routes.draw do
   namespace :instructor do
     resources :lessons, only: [:update]
     resources :sections, only: [:update] do
-      resources :lessons, only: [:new, :create]
+      resources :lessons, only: [:new, :create] do
     end
+  end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
     end
